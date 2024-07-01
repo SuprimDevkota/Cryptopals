719e9d43dacac28eafcfb82ae7636285c960aac6
Suprim Devkota <suprimdevkota@gmail.com>
2024-07-01 11:05:08 +0545

    Set 1 Question 2

---
+++ Set1_Basics/Q2_Fixed_XOR.py
@@ -0,0 +1,13 @@
+def fixed_xor(f: bytes, s: bytes) -> str:
+    if len(f) != len(s):
+        print("Byte strings are of different length")
+        return 1
+    r = ''.join([chr(fb^ sb) for fb, sb in zip(f, s)]).encode().hex()
+    return r
+
+if __name__ == "__main__":
+    first_string = "1c0111001f010100061a024b53535009181c"
+    second_string = "686974207468652062756c6c277320657965"
+
+    print(fixed_xor(bytes.fromhex(first_string), bytes.fromhex(second_string)))
+

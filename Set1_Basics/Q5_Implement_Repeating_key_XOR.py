61b5792a79e8df1726ef6010cb05c345a2c713ee
Suprim Devkota <suprimdevkota@gmail.com>
2024-07-03 08:29:43 +0545

    Modified Q5 to accomodate Q6

--- Set1_Basics/Q5_Implement_Repeating_key_XOR.py
+++ Set1_Basics/Q5_Implement_Repeating_key_XOR.py
@@ -5,7 +5,7 @@ def repeating_key_xor(ciphertext: bytes, key: str) -> bytes:
     for byte in ciphertext:
         count = (count + 1) % len(key_list)
         result += bytes([byte ^ key_list[count]])
-    return result.hex()
+    return result
 
 if __name__ == "__main__":
     string = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
@@ -13,4 +13,4 @@ if __name__ == "__main__":
     byte_string = bytes(string, encoding='utf-8')
     key = "ICE"
     plaintext = repeating_key_xor(byte_string, key)
-    print(plaintext)
+    print(plaintext.hex())

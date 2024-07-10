598561aca3c3f2e4d9357091855d07bf51c6c134
Suprim Devkota <suprimdevkota@gmail.com>
2024-07-10 17:46:58 +0545

    Refactored Q2 and added Q3

--- Set2_Block_Crypto/Q2_Implement_CBC_mode.py
+++ Set2_Block_Crypto/Q2_Implement_CBC_mode.py
@@ -1,14 +1,14 @@
 import sys
-sys.path.append("../")
-from utils.my_utils import AES, decode, chunker, decrypt_aes, fixed_xor
+sys.path.append("../") 
+from utils.my_utils import AES, decode, chunker, decrypt_aes_ecb, fixed_xor
 
-def decrypt_cbc(cipher_text: bytes, key: bytes, block_size: int, iv: 0) -> bytes:
+def decrypt_cbc(cipher_text: bytes, key: bytes, block_size: int, iv: bytes) -> bytes:
     
     chunked_bytestrings = chunker(cipher_text, block_size)
     plaintext = b''
 
     for bytestring in chunked_bytestrings:
-        decrypted_ct = decrypt_aes(bytestring, key)
+        decrypted_ct = decrypt_aes_ecb(bytestring, key)
         plaintext += fixed_xor(decrypted_ct, iv)
         iv = bytestring
 
